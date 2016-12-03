a901ff9fb0ee237ca278fd91c9ff6b5e031998ea
Veselov <veselov.ilya.nn@gmail.com>
2016-12-03 18:02:44 +0300

    Detalized interfaces of main classes

--- FlowchartEditorMVP/FlowchartEditorMVP/Model/OrientedGraph.cs
+++ FlowchartEditorMVP/FlowchartEditorMVP/Model/OrientedGraph.cs
@@ -8,5 +8,23 @@ namespace FlowchartEditorMVP.Model
 {
     class OrientedGraph
     {
+        public void AddEdge(Node outNode, Node inNode)
+        {
+        }
+
+        public void AddNode()
+        {
+        }
+    }
+
+    class Node
+    {
+
+    }
+
+    class Edge
+    {
+        Node outNode;
+        Node inNode;
     }
 }
