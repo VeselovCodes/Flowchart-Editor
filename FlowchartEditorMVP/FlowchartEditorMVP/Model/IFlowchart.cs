7a56c72fabc192fa4ba2711aa0dade225162f394
Павел <inbox40@inbox.ru>
2016-12-13 20:41:37 +0300

    refreshed project

--- FlowchartEditorMVP/FlowchartEditorMVP/Model/IFlowchart.cs
+++ FlowchartEditorMVP/FlowchartEditorMVP/Model/IFlowchart.cs
@@ -9,23 +9,40 @@ namespace FlowchartEditorMVP.Model
     interface IFlowchart
     {
         void AddBlock(IBlock block, Edge edge);
-        void AddStrToBlock(SquareBlock block, string str);
+        void AddStrToBlock(IBlock block, string str);
         void DeleteSquareBlock(SquareBlock block);
-
+        OrientedGraph getGraph();
     }
 
     class Flowchart : IFlowchart
     {
-        public void AddBlock(IBlock block, Edge edge)
+        private OrientedGraph graph;
+        private List<IBlock> blocks;
+
+        public Flowchart(int maxN)
         {
+            graph = new OrientedGraph();
+            blocks = new List<IBlock>();
         }
 
-        public void AddStrToBlock(SquareBlock block, string str)
+        public void AddBlock(IBlock block, Edge edge)
         {
+            blocks.Add(block);
+            graph.addEdge(edge);
+        }
+
+        public void AddStrToBlock(IBlock block, string str)
+        {
+            block.AddStr(str);
         }
 
         public void DeleteSquareBlock(SquareBlock block)
         {
+        }
+
+        public OrientedGraph getGraph()
+        {
+            return graph;
         }
     }
 }
--- FlowchartEditorMVP/FlowchartEditorMVP/View/ChooseFlowchartView.Designer.cs
+++ FlowchartEditorMVP/FlowchartEditorMVP/View/ChooseFlowchartView.Designer.cs
@@ -28,11 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowchartDataGridView = new System.Windows.Forms.DataGridView();
+            this.flowchartColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loginColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openButton = new System.Windows.Forms.Button();
+            this.createNewButton = new System.Windows.Forms.Button();
+            this.changeUserButton = new System.Windows.Forms.Button();
+            this.excLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.flowchartDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowchartDataGridView
+            // 
+            this.flowchartDataGridView.AllowUserToAddRows = false;
+            this.flowchartDataGridView.AllowUserToDeleteRows = false;
+            this.flowchartDataGridView.AllowUserToResizeRows = false;
+            this.flowchartDataGridView.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.flowchartDataGridView.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.flowchartDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.flowchartDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.flowchartColumn,
+            this.loginColumn});
+            this.flowchartDataGridView.Location = new System.Drawing.Point(300, 50);
+            this.flowchartDataGridView.Name = "flowchartDataGridView";
+            this.flowchartDataGridView.Size = new System.Drawing.Size(400, 600);
+            this.flowchartDataGridView.TabIndex = 0;
+            this.flowchartDataGridView.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.flowchartDataGridView_CellClick);
+            // 
+            // flowchartColumn
+            // 
+            this.flowchartColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.flowchartColumn.HeaderText = "Flowchart";
+            this.flowchartColumn.Name = "flowchartColumn";
+            // 
+            // loginColumn
+            // 
+            this.loginColumn.HeaderText = "Login";
+            this.loginColumn.Name = "loginColumn";
+            this.loginColumn.Width = 150;
+            // 
+            // openButton
+            // 
+            this.openButton.Enabled = false;
+            this.openButton.Location = new System.Drawing.Point(715, 50);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(122, 43);
+            this.openButton.TabIndex = 1;
+            this.openButton.Text = "Open";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // createNewButton
+            // 
+            this.createNewButton.Location = new System.Drawing.Point(849, 50);
+            this.createNewButton.Name = "createNewButton";
+            this.createNewButton.Size = new System.Drawing.Size(122, 43);
+            this.createNewButton.TabIndex = 1;
+            this.createNewButton.Text = "Create new";
+            this.createNewButton.UseVisualStyleBackColor = true;
+            this.createNewButton.Click += new System.EventHandler(this.createNewButton_Click);
+            // 
+            // changeUserButton
+            // 
+            this.changeUserButton.Location = new System.Drawing.Point(84, 50);
+            this.changeUserButton.Name = "changeUserButton";
+            this.changeUserButton.Size = new System.Drawing.Size(122, 43);
+            this.changeUserButton.TabIndex = 1;
+            this.changeUserButton.Text = "Change user";
+            this.changeUserButton.UseVisualStyleBackColor = true;
+            this.changeUserButton.Click += new System.EventHandler(this.changeUserButton_Click);
+            // 
+            // excLabel
+            // 
+            this.excLabel.AutoSize = true;
+            this.excLabel.Location = new System.Drawing.Point(712, 96);
+            this.excLabel.Name = "excLabel";
+            this.excLabel.Size = new System.Drawing.Size(0, 13);
+            this.excLabel.TabIndex = 2;
+            // 
+            // ChooseFlowchartView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ChooseFlowchartView";
+            this.ClientSize = new System.Drawing.Size(984, 335);
+            this.Controls.Add(this.excLabel);
+            this.Controls.Add(this.createNewButton);
+            this.Controls.Add(this.changeUserButton);
+            this.Controls.Add(this.openButton);
+            this.Controls.Add(this.flowchartDataGridView);
+            this.Name = "ChooseFlowchartView";
+            this.Text = "Flowchart Editor";
+            this.Load += new System.EventHandler(this.ChooseFlowchartView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.flowchartDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView flowchartDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn flowchartColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loginColumn;
+        private System.Windows.Forms.Button openButton;
+        private System.Windows.Forms.Button createNewButton;
+        private System.Windows.Forms.Button changeUserButton;
+        private System.Windows.Forms.Label excLabel;
     }
 }
